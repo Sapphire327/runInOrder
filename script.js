0b55a0b9b4dc46fe468a4a26b00395908fd5b79b
Сергей <nochnoy_briz@mail.ru>
2024-12-03 14:59:40 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function runInOrder(functions, delays) {
+    for (let i = 0; i < functions.length; i++) {
+        setTimeout(functions[i], delays[i]);
+    }
+}
+const functions = [() => console.log("first"), () => console.log("second"), () => console.log("third")];
+const delays = [2000, 1000, 3000];
+
+runInOrder(functions, delays); // logs "first" after 2 seconds, "second" after another 1 second, and "third" after another 3 seconds
